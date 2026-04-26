41a236d72c18124d8210284b80b7e0dbf15b7f14
KaungSettThu1873 <kaungsettthu1873@example.com>
2026-04-27 03:02:10 +0630

    Feat: add receipe store api

--- backend/Controllers/ReceipeController.js
+++ backend/Controllers/ReceipeController.js
@@ -1,13 +1,42 @@
+const Receipe = require("../Models/Receipe");
+
 const ReceipeController = {
     index : (req,res) => {
         return res.json({
             "message" : "Get success"
         });
     },
-    store : (req,res) => {
-        return res.json({
-            "message" : "Store success"
-        })
+    store : async (req,res) => {
+        try
+        {
+            let {title,description,ingredients} = req.body;
+
+            if (!ingredients || ingredients.length === 0) {
+                return res.status(422).json({
+                    success: false,
+                    message: "Ingredients cannot be empty"
+                }); 
+            }
+
+            const data = await Receipe.create({
+                title,
+                description,
+                ingredients
+            });
+            
+            return res.status(201).json({
+                "success" : true,
+                "message" : "Store success",
+                data,
+            })
+        }
+        catch(e)
+        {
+            return res.status(500).json({
+                "success" : false,
+                "message" : e.message || "Internal server error",
+            })
+        }
     },
     show : (req,res) => {
         return res.json({
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 require('dotenv').config();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const receipesRoute = require('./Routes/receipes')
 const app = express();
+app.use(express.json())
 app.use(morgan('dev'));
+
+mongoose.connect(process.env.DB_URL).then(() => {
+    console.log("DB connected");
+    app.listen(process.env.PORT,() => {
+        console.log("App is running on PORT "+process.env.PORT);
+    });
+})
+   
+
 
 app.get('/',(req,res)=>{
     return res.json({
@@ -12,6 +23,3 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/receipes',receipesRoute);
-app.listen(process.env.PORT,() => {
-    console.log("App is running on PORT "+process.env.PORT);
-})
